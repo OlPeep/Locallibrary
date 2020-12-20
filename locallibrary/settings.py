a8aefb98685294c389c2b3d8274d6cc07ae21fc2
OlPeep <evilmall@mail.ru>
2020-12-20 20:38:21 +0300

    Allowed Hosts

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -31,7 +31,7 @@ DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 #export DJANGO_DEBUG=False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['safe-temple-25840.herokuapp.com']
 
 
 # Application definition
